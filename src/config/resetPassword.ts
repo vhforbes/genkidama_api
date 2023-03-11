b67a3813a146a7619418720f8f3460eb28d45dd5
Victor Forbes <vhforbes@gmail.com>
2023-03-11 12:08:42 -0300

    Versioning untracked files

--- src/config/resetPassword.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  jwt: {
-    secret: '11239asd23d1asd6b260d356easf',
-    expiresIn: '10m',
-  },
-};
--- src/interfaces/ExclusiveVideoInterface.ts
+++
@@ -1,7 +0,0 @@
-export interface ExclusiveVideoInterface {
-  authorId: string;
-  title: string;
-  content: string;
-  image: string;
-  videoLink: string;
-}
