c8bfecc8b70ee6de47fa573992ea46db083c42e5
Victor Forbes <vhforbes@gmail.com>
2023-08-31 09:38:36 -0300

    fix: message to users fix loop

--- src/bot/utils/sendMessageToUsers.ts
+++ src/bot/utils/sendMessageToUsers.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 import User from '../../models/User';
 import { bot } from '../initializeBot';
 
@@ -7,17 +8,26 @@ interface Request {
 }
 
 const sendMessageToUsers = async ({ users, messageHtml }: Request) => {
-  users.forEach(async (user: User) => {
+  for (const user of users) {
     if (user?.telegramId) {
       try {
+        // eslint-disable-next-line no-await-in-loop
         await bot.sendMessage(user.telegramId, messageHtml, {
           parse_mode: 'HTML',
         });
-      } catch (err) {
-        console.error(`Cloud not send message to ${user.email}`);
+      } catch (err: any) {
+        if (err.code === 'ETELEGRAM') {
+          console.error(
+            `Could not send message to ${user.email} because the bot was blocked by the user`,
+          );
+        } else {
+          console.error(
+            `Could not send message to ${user.email} due to an unknown error: ${err}`,
+          );
+        }
       }
     }
-  });
+  }
 };
 
 export default sendMessageToUsers;
